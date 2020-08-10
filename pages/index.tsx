2b7ad17a0e4dec112b19087c037610f2cde78912
Corey Scheideman <corscheid@gmail.com>
2020-08-10 14:16:57 -0400

    Convert to TypeScript

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,21 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import Head from 'next/head'
+import Link from 'next/link'
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  const name = "Corey Scheideman";
-  const tagline = "Homemade Dev";
-  const title = `${name} | Portfolio`;
+  const name = 'Corey Scheideman'
+  const tagline = 'Homemade Dev'
+  const title = `${name} | Portfolio`
   return (
     <div>
       <Head>
         <title>{title}</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <header className={styles.header}>
         <img
-          src="/images/profile.jpg"
+          src='/images/profile.jpg'
           className={`${styles.headerImage} ${styles.borderCircle}`}
           alt={name}
         />
@@ -22,11 +23,15 @@ export default function Home() {
           <h1 className={styles.name}>{name}</h1>
           <small>{tagline}</small>
           <nav>
-            <a>Home</a>
-            {" / "}
-            <a>About</a>
-            {" / "}
+            <Link href='/'>
+              <a>Home</a>
+            </Link>
+            {' / '}
+            <a>Blog</a>
+            {' / '}
             <a>Projects</a>
+            {' / '}
+            <a>Contact</a>
           </nav>
         </div>
       </header>
@@ -35,5 +40,5 @@ export default function Home() {
         <p>Hello, World!</p>
       </main>
     </div>
-  );
+  )
 }
