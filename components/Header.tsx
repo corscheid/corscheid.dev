f132f753ef79695f80c182803d34875024e0755a
Corey Scheideman <corscheid@gmail.com>
2020-08-11 18:32:46 -0400

    Format to 2 spaces tab width for consistency

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,20 +2,20 @@ import Navigation from './Navigation'
 import styles from './Header.module.css'
 
 export default function Header() {
-    const name = 'Corey Scheideman'
-    const tagline = 'Homemade Dev'
-    return (
-        <header className={styles.header}>
-            <img
-                src='/images/profile.jpg'
-                className={`${styles.headerImage} ${styles.borderCircle}`}
-                alt={name}
-            />
-            <div className={styles.headerContainer}>
-                <h1 className={styles.name}>{name}</h1>
-                <small>{tagline}</small>
-                <Navigation />
-            </div>
-        </header>
-    )
+  const name = 'Corey Scheideman'
+  const tagline = 'Homemade Dev'
+  return (
+    <header className={styles.header}>
+      <img
+        src='/images/profile.jpg'
+        className={`${styles.headerImage} ${styles.borderCircle}`}
+        alt={name}
+      />
+      <div className={styles.headerContainer}>
+        <h1 className={styles.name}>{name}</h1>
+        <small>{tagline}</small>
+        <Navigation />
+      </div>
+    </header>
+  )
 }
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,22 +4,22 @@ import Header from './Header'
 import Footer from './Footer'
 
 interface Props {
-    title: string;
-    children: React.ReactNode;
+  title: string;
+  children: React.ReactNode;
 }
 
 export default function Layout({ title, children }: Props) {
-    return (
-        <>
-            <Head>
-                <meta charSet="utf-8" />
-                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-                <title>{title}</title>
-                <link rel='icon' href='/favicon.ico' />
-            </Head>
-            <Header />
-            <main className="container">{children}</main>
-            <Footer />
-        </>
-    )
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <title>{title}</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <Header />
+      <main className="container">{children}</main>
+      <Footer />
+    </>
+  )
 }
