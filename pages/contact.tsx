6b102e198831a788e4f7f4fee94f9cc749549560
Corey Scheideman <corscheid@gmail.com>
2020-08-12 05:26:20 -0400

    Add basic contact starter page

---
+++ pages/contact.tsx
@@ -0,0 +1,15 @@
+import Layout from '../components/Layout';
+
+export default function Contact() {
+  return (
+    <Layout title="Contact">
+      <h1>Contact</h1>
+      <p>Let's get in touch! Here's how you can reach me:</p>
+      <ul style={{ listStyleType: 'initial' }}>
+        <li>Twitter: <a href="https://twitter.com/corscheid">@corscheid</a></li>
+        <li>LinkedIn: <a href="https://linkedin.com/in/corscheid">corscheid</a></li>
+        <li>Keybase: <a href="https://keybase.io/corscheid">corscheid</a></li>
+      </ul>
+    </Layout>
+  );
+}
