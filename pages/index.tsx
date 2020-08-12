9dc6724d9d74c8e9731922ac706d9559d233a0b8
Corey Scheideman <corscheid@gmail.com>
2020-08-12 12:26:30 -0400

    minor reformatting, resize brackets

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,11 @@ export default function Home() {
   return (
     <Layout title="Home">
       <style jsx>{`
-      .big-text {
-        font-size: 6rem;
-        text-align: center;
-        margin-bottom: 2rem;
-      }
+        .big-text {
+          font-size: 4rem;
+          text-align: center;
+          margin-bottom: 2rem;
+        }
       `}</style>
       <div className="big-text">
         &#60; &#47;&#62; &#123; &#125;
@@ -23,13 +23,15 @@ export default function Home() {
         and frameworks outside of my university studies.
       </p>
       <p>
-        A lot of the development I have done thus far has been for the <a href="https://LearnNavi.org/">LearnNa'vi</a> Community
-        using the alias "Tirea Aean" (Na'vi for <em>Blue Spirit'</em>) or "tirea" (<em>spirit</em>). Learning and teaching the Na'vi language from
-        James Cameron's AVATAR has been a fun hobby for me since January 2010. It was also how I found my way towards
-        choosing to become a Software Engineer as my career goal.
+        A lot of the development I have done thus far has been for the <a href="https://LearnNavi.org/">LearnNa'vi</a>
+        Community using the alias "Tirea Aean" (Na'vi for <em>Blue Spirit'</em>) or "tirea" (<em>spirit</em>). Learning
+        and teaching the Na'vi language from James Cameron's AVATAR has been a fun hobby for me since January 2010. It
+        was also how I found my way towards choosing to become a Software Engineer as my career goal.
       </p>
       <p>
-        Some of my favorite technologies to work with these days are <a href="https://reactjs.org/">React.js</a>, <a href="https://reactnative.dev/">React Native</a>, <a href="https://developer.android.com/">Android</a>, <a href="https://golang.org/">Go</a>, and <a href="https://www.python.org/">Python</a>.
+        Some of my favorite technologies to work with these days are <a href="https://reactjs.org/">React.js</a>,&nbsp;
+        <a href="https://reactnative.dev/">React Native</a>, <a href="https://developer.android.com/">Android</a>,&nbsp;
+        <a href="https://golang.org/">Go</a>, and <a href="https://www.python.org/">Python</a>.
       </p>
     </Layout>
   )
