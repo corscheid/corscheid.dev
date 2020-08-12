35d19ddf122747352f441e2cfec6ed8d6d797eec
Corey Scheideman <corscheid@gmail.com>
2020-08-12 05:25:11 -0400

    Update nav to better show active item

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,19 +1,41 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+interface NavItem {
+  name: string
+  dest: string
+}
+
+const links: NavItem[] = [
+  { name: 'Home', dest: '/' },
+  { name: 'Blog', dest: '/blog' },
+  { name: 'Projects', dest: '/projects' },
+  { name: 'Contact', dest: '/contact' }
+]
 
 export default function Navigation() {
+  const { pathname } = useRouter();
   return (
     <nav>
-      <Link href='/'>
-        <a>Home</a>
-      </Link>
-      {' / '}
-      <Link href="/blog">
-        <a>Blog</a>
-      </Link>
-      {' / '}
-      <a>Projects</a>
-      {' / '}
-      <a>Contact</a>
+      <style jsx>{`
+        .nav-link {
+          font-size: 18px;
+        }
+        .active {
+          font-weight: bold;
+          border-bottom: 2px solid;
+        }
+      `}</style>
+      {links.map(({ name, dest }) => (
+        <>
+          <Link href={dest}>
+            <a className={pathname === dest ? 'active nav-link' : 'nav-link'}>
+              {name}
+            </a>
+          </Link>
+          {name !== 'Contact' ? ' / ' : null}
+        </>
+      ))}
     </nav>
   )
 }
