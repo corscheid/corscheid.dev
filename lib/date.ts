335a2a948d6d5db105e4e3d2c83ee2578e2e9b90
Corey Scheideman <corscheid@gmail.com>
2020-08-12 14:48:28 -0400

    Fix day numbers

--- lib/date.ts
+++ lib/date.ts
@@ -4,6 +4,8 @@ export function formatDate(dateString: string): string {
     const month = postDate.getMonth() + 1 < 10
         ? '0' + (postDate.getMonth() + 1)
         : postDate.getMonth() + 1;
-    const day = postDate.getDate();
+    const day = postDate.getDate() < 10
+        ? '0' + (postDate.getDate())
+        : postDate.getDate()
     return `${year}-${month}-${day}`;
 }
