5776ee437e8014663d314a58422883aadd60fdec
Corey Scheideman <corscheid@gmail.com>
2020-08-12 16:07:27 -0400

    Sort repos by created_at date descending

--- lib/github.ts
+++ lib/github.ts
@@ -7,6 +7,17 @@ export interface GitHubRepository {
   created_at: string;
 }
 
+function compareProjDates(a: GitHubRepository, b: GitHubRepository): number {
+  const aDate = new Date(a.created_at)
+  const bDate = new Date(b.created_at)
+  // sort descending
+  if (aDate > bDate) {
+    return -1
+  } else {
+    return 1
+  }
+}
+
 export async function getRepositories(): Promise<GitHubRepository[]> {
   let repositories: GitHubRepository[];
   if (fs.existsSync('projects.json')) {
@@ -17,5 +28,5 @@ export async function getRepositories(): Promise<GitHubRepository[]> {
     repositories = await response.json()
   }
   // TODO: sort by date descending
-  return repositories
+  return repositories.sort((a, b) => compareProjDates(a, b))
 }
