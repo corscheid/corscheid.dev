dae80750a5929cb0eb7767e9ba102189b6ccedcf
Corey Scheideman <corscheid@gmail.com>
2020-08-14 03:03:17 -0400

    Update github.ts Include all of my repos, across GitHub accounts and organization. Exclude duplicates and deprecated projects.

--- lib/github.ts
+++ lib/github.ts
@@ -19,18 +19,42 @@ function compareProjDates(a: GitHubRepository, b: GitHubRepository): number {
 }
 
 export async function getRepositories(): Promise<GitHubRepository[]> {
-  let repositories: GitHubRepository[];
-  const exclude = ['corscheid.github.io', 'nextjs-blog', 'php-sqlite-login']
+  let repositories: GitHubRepository[]
+  let repos_corscheid: GitHubRepository[]
+  let repos_tirea: GitHubRepository[]
+  let repos_fwew: GitHubRepository[]
+
+  const exclude = {
+    corscheid: ['corscheid.github.io', 'fwew', 'fwew-api', 'fwew-react', 'nextjs-blog', 'php-sqlite-login'],
+    tirea: ['fwew', 'fwew-discord', 'LearnNavi', 'ralpengyu', 'Tree-of-Voices-Online'],
+    // fwew: [],
+  }
 
   if (fs.existsSync('projects.json')) {
     const json = await fs.promises.readFile('./projects.json', 'utf8')
-    repositories = JSON.parse(json);
+    repositories = JSON.parse(json)
   } else {
-    const response = await fetch('https://api.github.com/users/corscheid/repos')
-    repositories = await response.json()
+    repositories = []
+
+    const res_corscheid = await fetch('https://api.github.com/users/corscheid/repos')
+    repos_corscheid = await res_corscheid.json()
+    repos_corscheid
+      .filter(repository => !exclude.corscheid.includes(repository.name))
+      .forEach(repo => { repositories.push(repo) })
+
+    const res_tirea = await fetch('https://api.github.com/users/tirea/repos')
+    repos_tirea = await res_tirea.json()
+    repos_tirea
+      .filter(repository => !exclude.tirea.includes(repository.name))
+      .forEach(repo => { repositories.push(repo) })
+
+    const res_fwew = await fetch('https://api.github.com/orgs/fwew/repos')
+    repos_fwew = await res_fwew.json()
+    repos_fwew
+      // .filter(repository => !exclude.fwew.includes(repository.name))
+      .forEach(repo => { repositories.push(repo) })
+
     await fs.promises.writeFile('./projects.json', JSON.stringify(repositories), 'utf-8')
   }
-  return repositories
-    .filter(repository => !exclude.includes(repository.name))
-    .sort((a, b) => compareProjDates(a, b))
+  return repositories.sort((a, b) => compareProjDates(a, b))
 }
