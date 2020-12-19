50526f7d4722610f8b9185c10a79200e42cd601e
Corey Scheideman <corscheid@gmail.com>
2020-12-19 17:42:08 -0600

    add meta description

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -14,6 +14,8 @@ export default function Layout({ title, children }: Props) {
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="Description" content="I'm Corey Scheideman and I'm a recent graduate with a Bachelor's Degree in Computer Science looking to start my career
+        in the Saint Louis, Missouri area." />
         <title>{title}</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
