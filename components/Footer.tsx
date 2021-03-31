2229ca6378927cb82e6d90d3c9eaee03f0f2e25f
Corey Scheideman <corscheid@gmail.com>
2021-03-31 01:00:44 -0400

    use map index for key, use single quotes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,19 +13,21 @@ import FooterIconLink from './FooterIconLink'
 
 export default function Footer() {
   const links: Hyperlink[] = [
-    { title: "dev", href: DEV_URL },
-    { title: "keybase", href: KEYBASE_URL },
-    { title: "twitter", href: TWITTER_URL },
-    { title: "github", href: GITHUB_URL },
-    { title: "linkedin", href: LINKEDIN_URL },
-    { title: "facebook", href: FACEBOOK_URL }
+    { title: 'dev', href: DEV_URL },
+    { title: 'keybase', href: KEYBASE_URL },
+    { title: 'twitter', href: TWITTER_URL },
+    { title: 'github', href: GITHUB_URL },
+    { title: 'linkedin', href: LINKEDIN_URL },
+    { title: 'facebook', href: FACEBOOK_URL }
   ]
   return (
     <footer>
       <div className={styles.container}>
         <div className={styles.flex}>
           <div>
-            {links.map(link => <FooterIconLink title={link.title} href={link.href} key={link.title} />)}
+            {links.map((link, idx) => (
+              <FooterIconLink title={link.title} href={link.href} key={idx} />
+            ))}
           </div>
           <div>
             <a className={styles.footerLink} href={GITHUB_URL}>
