cad8d92fbc144ce8eeeca53246ec953e1c6fe35a
Corey Scheideman <corscheid@gmail.com>
2021-04-04 05:07:52 -0400

    Use Context

--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -1,52 +1,34 @@
-import { useEffect, useState } from 'react'
+import { ThemeContext } from '../lib/theme-context'
 
 export default function ThemeSwitcher() {
-  // TODO: Fix behavior when switching themes between switching pages
-  const [theme, setTheme] = useState('')
-  const [iconClassName, setIconClassName] = useState('')
-
-  useEffect(() => {
-    const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches
-    setTheme(isDark ? 'dark' : 'light')
-    setIconClassName(isDark ? 'fas fa-moon' : 'far fa-sun')
-  }, [])
-
-  const switchTheme = () => {
-    if (theme === 'dark') {
-      document.documentElement.dataset.theme = 'light'
-      setTheme('light')
-      setIconClassName('far fa-sun')
-    } else {
-      document.documentElement.dataset.theme = 'dark'
-      setTheme('dark')
-      setIconClassName('fas fa-moon')
-    }
-  }
-
   return (
-    <>
-      <style jsx global>{`
-        #theme-toggle {
-          width: 4.5em;
-          color: var(--nc-lk-1);
-          cursor: pointer;
-          -webkit-touch-callout: none;
-          -webkit-user-select: none;
-          -khtml-user-select: none;
-          -moz-user-select: none;
-          -ms-user-select: none;
-          -o-user-select: none;
-          user-select: none;
-        }
-        #theme-toggle i {
-          padding: 0.25em;
-          padding-left: 0;
-        }
-      `}</style>
-      <div id="theme-toggle" onClick={switchTheme}>
-        <i className={iconClassName}></i>
-        Theme
-      </div>
-    </>
+    <ThemeContext.Consumer>
+      {({ icon, toggle }) => (
+        <>
+          <style jsx global>{`
+            #theme-toggle {
+              width: 4.5em;
+              color: var(--nc-lk-1);
+              cursor: pointer;
+              -webkit-touch-callout: none;
+              -webkit-user-select: none;
+              -khtml-user-select: none;
+              -moz-user-select: none;
+              -ms-user-select: none;
+              -o-user-select: none;
+              user-select: none;
+            }
+            #theme-toggle i {
+              padding: 0.25em;
+              padding-left: 0;
+            }
+          `}</style>
+          <div id="theme-toggle" onClick={toggle}>
+            <i className={icon}></i>
+            Theme
+          </div>
+        </>
+      )}
+    </ThemeContext.Consumer>
   )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,35 @@ import 'highlight.js/styles/default.css'
 import '../styles/atom-one-dark.css'
 import '../styles/globals.css'
 
+import React, { useState } from 'react'
+
 import type { AppProps } from 'next/app'
+import { ThemeContext } from '../lib/theme-context'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const toggleTheme = () => {
+    setState((state) => {
+      const newTheme = state.theme === 'dark' ? 'light' : 'dark'
+      const newIcon = state.theme === 'dark' ? 'far fa-sun' : 'fas fa-moon'
+      document.documentElement.dataset.theme = newTheme
+      console.log({ newTheme, newIcon })
+      return {
+        ...state,
+        theme: newTheme,
+        icon: newIcon
+      }
+    })
+  }
+  const initialState = {
+    theme: 'dark',
+    icon: 'fas fa-moon',
+    toggle: toggleTheme
+  }
+  const [state, setState] = useState(initialState)
+
+  return (
+    <ThemeContext.Provider value={state}>
+      <Component {...pageProps} />
+    </ThemeContext.Provider>
+  )
 }
