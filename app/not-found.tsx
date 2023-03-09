64fa853989b2f165392a4084f95623087fbbd7bd
Corey Scheideman <corscheid@gmail.com>
2023-03-09 03:20:23 -0600

    Add NotFound component for when 404s catch up

---
+++ app/not-found.tsx
@@ -0,0 +1,16 @@
+export default function NotFound() {
+  return (
+    <div style={{ display: 'grid', placeItems: 'center' }}>
+      <pre>{`
+ ________________________________
+< 404 | Page could not be found >
+ --------------------------------
+        \\   ^__^
+         \\  (oo)\\_______
+            (__)\\       )\\/\\
+                ||----w |
+                ||     ||
+      `}</pre>
+    </div>
+  )
+}
