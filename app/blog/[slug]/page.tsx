f3be8942ab0b4a4bfceafefc5b989672721fd009
Corey Scheideman <corscheid@gmail.com>
2023-03-09 20:47:02 -0600

    Rejoin Blog and Post to their respective pages

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,7 +1,8 @@
-import Post from '@/components/Post'
-import type { Params, PostProps } from '@/interfaces'
+import type { Params } from '@/interfaces'
+import { formatDate } from '@/lib/date'
 import { markdownToHtml } from '@/lib/markdown'
 import { getPosts } from '@/lib/posts'
+import Link from 'next/link'
 import { notFound } from 'next/navigation'
 
 export default async function Page({ params }: Params) {
@@ -12,7 +13,42 @@ export default async function Page({ params }: Params) {
   }
   const { title, date, content } = post
   const html = markdownToHtml(content)
-  const props: PostProps = { slug, title, date, html }
 
-  return <Post {...props} />
+  return (
+    <article>
+      <style>{`
+          .article-header h1 {
+            margin-bottom: 8px;
+          }
+          .article-date {
+            fontsize: 1em;
+            margin-bottom: 2rem;
+            text-align: center;
+          }
+        `}</style>
+      <div className="article-header">
+        <h1 itemProp="name headline">{title}</h1>
+        <p className="article-date">
+          <time dateTime={date} itemProp="datePublished">
+            {formatDate(date)}
+          </time>
+        </p>
+      </div>
+
+      <div
+        className="main-content"
+        itemProp="articleBody"
+        dangerouslySetInnerHTML={{ __html: html }}
+      ></div>
+
+      <a href={`/blog/${slug}`} hidden></a>
+
+      <a href="#">&uarr; Up to top</a>
+      <br />
+      <br />
+      <Link href="/blog" legacyBehavior>
+        <a>&larr; Back to blog</a>
+      </Link>
+    </article>
+  )
 }
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,15 +1,61 @@
-import Blog from '@/components/Blog'
-import { DOMAIN } from '@/lib/constants'
+import { BLOG, DOMAIN } from '@/lib/constants'
+import { formatDate } from '@/lib/date'
 import { getPosts } from '@/lib/posts'
+import marked from 'marked'
+import Image from 'next/legacy/image'
+import Link from 'next/link'
 
 export const metadata = {
-  title: 'Blog',
-  description: `Blog | ${DOMAIN}`
+  title: BLOG,
+  description: `${BLOG} | ${DOMAIN}`
 }
 
 export default async function Page() {
   const posts = await getPosts()
   posts.sort((a, b) => b.date.localeCompare(a.date))
 
-  return <Blog posts={posts} />
+  return (
+    <>
+      <h1>{BLOG}</h1>
+      {posts.map(
+        ({ slug, title, date, cover_image, cover_alt, content }, idx) => (
+          <article key={slug}>
+            <h2>
+              <Link href="/blog/[slug]" as={`/blog/${slug}`} legacyBehavior>
+                <a>{title}</a>
+              </Link>
+            </h2>
+            <p className="post-meta">
+              <time
+                className="dt-published"
+                dateTime={date}
+                itemProp="datePublished"
+              >
+                {formatDate(date)}
+              </time>
+            </p>
+            <Image
+              src={cover_image}
+              alt={cover_alt}
+              width={750}
+              height={420}
+              priority={idx < 5}
+              loading={idx < 5 ? 'eager' : 'lazy'}
+            />
+            <div
+              className="post-content e-content"
+              itemProp="articleBody"
+              dangerouslySetInnerHTML={{
+                __html: marked(content.split('\n')[3])
+              }}
+            ></div>
+            <Link href="/blog/[slug]" as={`/blog/${slug}`} legacyBehavior>
+              <a>Read more &rarr;</a>
+            </Link>
+            <hr />
+          </article>
+        )
+      )}
+    </>
+  )
 }
