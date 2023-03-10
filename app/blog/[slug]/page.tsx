0c6c012dc8ed55ebcec549e2a3133c6adbe3d8df
Corey Scheideman <corscheid@gmail.com>
2023-03-09 20:18:04 -0600

    fix blog post 404 handling

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -4,12 +4,13 @@ import Post from '@/components/Post'
 import type { Params, PostProps } from '@/interfaces'
 import { markdownToHtml } from '@/lib/markdown'
 import { getPosts } from '@/lib/posts'
+import { notFound } from 'next/navigation'
 
 export default async function Page({ params }: Params) {
   const { slug } = params
   const post = (await getPosts()).find((p) => p.slug === slug)
   if (!post) {
-    throw new Error(`Expected slug ${slug}`)
+    notFound()
   }
   const { title, date, content } = post
   const html = markdownToHtml(content)
