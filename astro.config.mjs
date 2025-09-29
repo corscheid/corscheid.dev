5b51be04a09d7b590705782d815a233ec0d52a50
Corey Scheideman <corscheid@gmail.com>
2025-09-29 03:50:59 -0500

    remove devtool-tw-breaks

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,10 @@ import playformCompress from '@playform/compress';
 import tailwindcss from '@tailwindcss/vite';
 import compressor from 'astro-compressor';
 import { defineConfig } from 'astro/config';
-import devtoolTwBreaks from "./devtool-tw-breaks/integration";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    devtoolTwBreaks,
     playformCompress({
       CSS: true,
       HTML: true,
