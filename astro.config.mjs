e271944973cebfb7bf3ac423b64dbcb31e43cd0c
Corey Scheideman <corscheid@gmail.com>
2025-09-29 02:43:26 -0500

    Remove vercel server adapter, site now completely static again

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 // @ts-check
-import vercel from '@astrojs/vercel';
 import playformCompress from '@playform/compress';
 import tailwindcss from '@tailwindcss/vite';
 import compressor from 'astro-compressor';
@@ -26,6 +25,4 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-
-  adapter: vercel(),
 });
